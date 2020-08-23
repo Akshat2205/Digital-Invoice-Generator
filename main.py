614a0cd800064b3deea51379258d40bb980cdc9b
bitakshat <akshatpal1232@gmail.com>
2020-08-23 14:40:46 +0530

    add reset button and changed gst variable scope line 20 & 25

--- main.py
+++ main.py
@@ -11,17 +11,18 @@ form 			= document['form']
 radio 			= document['gst']
 mastertotal 	= document['gtotal']
 submit 			= document['subbut']
+resetpage       = document['reset']
 
 mastertotal.value 		= 0
 productprice.value 		= 0
 productquantity.value 	= 0
 totalprice.value		= 0
-
+gst = 0
 
 @bind(form, "input")
 def update_value(event):
 
-	gst = 0
+	global gst
 	if radio.children[2].checked: gst = int(radio.children[2].value)
 	if radio.children[4].checked: gst = int(radio.children[4].value) 
 	if radio.children[6].checked: gst = int(radio.children[6].value)
@@ -32,3 +33,11 @@ def update_value(event):
 @bind(submit, "click")
 def print_slip(event):
 	console.log('print the slip from printer')
+
+@bind(resetpage, "click")
+def reset_values(ev):
+	mastertotal.value 		= 0
+	productprice.value 		= 0
+	productquantity.value 	= 0
+	totalprice.value		= 0
+	gst = 0
