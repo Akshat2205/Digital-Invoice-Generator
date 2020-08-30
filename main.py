b71a1642d8fab6aacca5cebe19e296b89f1a1786
bitakshat <akshatpal1232@gmail.com>
2020-08-30 23:58:51 +0530

    Repositioned elements label and inputs

--- main.py
+++ main.py
@@ -12,12 +12,18 @@ radio 			= document['gst']
 mastertotal 	= document['gtotal']
 submit 			= document['subbut']
 resetpage       = document['reset']
+products        = document['prodes']
+additem         = document['buttonitem']
+listitems       = document['listitems']
+
 
 mastertotal.value 		= 0
 productprice.value 		= 0
 productquantity.value 	= 0
 totalprice.value		= 0
 gst = 0
+item = ''
+
 
 @bind(form, "input")
 def update_value(event):
@@ -27,9 +33,13 @@ def update_value(event):
 	totalprice.value = int(productprice.value) * int(productquantity.value) 
 	mastertotal.value = int(totalprice.value) + int(totalprice.value) * int(gst) / 100
 
+'''
+
 @bind(submit, "click")
 def print_slip(event):
 	console.log('print the slip from printer')
+
+'''
 
 @bind(resetpage, "click")
 def reset_values(ev):
@@ -38,3 +48,12 @@ def reset_values(ev):
 	productquantity.value 	= 0
 	totalprice.value		= 0
 	gst = 0
+	item = ''
+
+
+@bind(additem, "click")
+def additem(ev):
+	global item
+	item = str(products.value)
+	console.log(item)
+	listitems.value = item
