b807c8a09a3d9f937b94bae57134a1eb6be5528b
bitakshat <akshatpal1232@gmail.com>
2020-09-03 14:06:04 +0530

    Changed UI and added Tax (IGST and SGST) function

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ productprice 	= document['price']
 productquantity = document['quantity']
 totalprice 		= document['total-value']
 form 			= document['form']
-radio 			= document['gst']
 mastertotal 	= document['gtotal']
 submit 			= document['subbut']
 resetpage       = document['reset']
 products        = document['prodes']
 additem         = document['buttonitem']
-listitems       = document['listitems']
 
+# tax variables
+tax   			= document['taxitem']
 
 mastertotal.value 		= 0
 productprice.value 		= 0
@@ -29,7 +29,10 @@ item = ''
 def update_value(event):
 
 	global gst
-	gst = int(radio.children[1].value)
+	gst = int(tax.children[0].children[0].value)
+	igst = int(tax.children[1].children[0].value)
+	cgst = int(tax.children[2].children[0].value)
+
 	totalprice.value = int(productprice.value) * int(productquantity.value) 
 	mastertotal.value = int(totalprice.value) + int(totalprice.value) * int(gst) / 100
 
@@ -41,13 +44,24 @@ def print_slip(event):
 
 '''
 
+# Reset page contents
 @bind(resetpage, "click")
 def reset_values(ev):
 	mastertotal.value 		= 0
 	productprice.value 		= 0
 	productquantity.value 	= 0
 	totalprice.value		= 0
+	
+	# tax
 	gst = 0
+	tax.children[0].children[0].value = 0
+	tax.children[1].children[0].value = 0
+	tax.children[2].children[0].value = 0
+
+	products.value = ""
+	
+	for x in range(0, 3):
+		tax.children[x].value = 0
 	item = ''
 
 
@@ -56,4 +70,4 @@ def additem(ev):
 	global item
 	item = str(products.value)
 	console.log(item)
-	listitems.value = item
+	products.value = item
